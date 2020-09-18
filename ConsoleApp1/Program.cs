8a9c1726fbb1725ec01ae1fb465e87738a661f34
Karolis Uosis <kuosis@alna.lt>
2020-09-18 09:54:52 +0300

    Pavyzdinis kodas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int b = TestCalculationMethod(1, 3);
+            Console.WriteLine(b);
+            Console.WriteLine("test");
             Console.WriteLine("Hello World!");
+        }
+
+        private static int TestCalculationMethod(int a1, int a3)
+        {
+
+
+            int a = 10;
+            int b = b + 5;
+            return b;
         }
     }
 }
